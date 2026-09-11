739d29944bdd6f565b4b4e20b13620080da85dfb
Prachi Pandey <pandeyprachi0172@gmail.com>
2026-09-11 18:13:14 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (20.45%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int ans = 0;
+
+        while (x != 0) {
+            int digit = x % 10;
+            x = x / 10;
+
+            if (ans > 214748364 || ans < -214748364) {
+                return 0;
+            }
+
+            ans = ans * 10 + digit;
+        }
+
+        return ans;
+    }
+};
